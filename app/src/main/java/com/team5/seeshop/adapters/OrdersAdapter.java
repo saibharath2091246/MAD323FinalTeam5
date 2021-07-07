bbd7ee207b2f15045bb99c570735359e14b4e1bd
Sana Shaikh <sanaks2797@gmail.com>
2021-07-07 12:00:52 -0400

    oRDER DETAILS AND ALL PRODUCTS ADDED

---
+++ app/src/main/java/com/team5/seeshop/adapters/OrdersAdapter.java
@@ -0,0 +1,103 @@
+package com.team5.seeshop.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.squareup.picasso.Picasso;
+import com.team5.seeshop.R;
+import com.team5.seeshop.customer.CustomerOrderDetailsActivity;
+import com.team5.seeshop.models.PlaceOrderModel;
+import com.team5.seeshop.utils.ConstantStrings;
+
+import java.util.List;
+
+public class OrdersAdapter extends RecyclerView.Adapter<OrdersAdapter.ViewHolder> {
+
+    List<PlaceOrderModel> productModelList;
+    Context context;
+    SharedPreferences sharedPref;
+    public OrdersAdapter(List<PlaceOrderModel> productModelList, Context context) {
+        this.productModelList = productModelList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.orders_row, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.title_tv.setText(productModelList.get(position).getCartItems().get(0).getTitle());
+        holder.price_tv.setText("$"+productModelList.get(position).getTotal_amount());
+        holder.order_id_tv.setText(productModelList.get(position).getOrder_id());
+        holder.status_tv.setText(""+productModelList.get(position).getOrder_status());
+        sharedPref = context.getSharedPreferences(ConstantStrings.SEESHOP_PREFS,0);
+
+
+        if (productModelList.get(position).getCartItems().get(0).getImages().size()>0)
+        {
+
+            Picasso.get().load(productModelList.get(position).getCartItems().get(0).getImages().get(0)).into(holder.image_iv);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, CustomerOrderDetailsActivity.class);
+                intent.putExtra("order_details",productModelList.get(position));
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productModelList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+         public TextView title_tv,price_tv,order_id_tv,status_tv;
+         ImageView image_iv;
+          public ViewHolder(View itemView) {
+            super(itemView);
+             title_tv = (TextView) itemView.findViewById(R.id.title_tv);
+            price_tv = (TextView) itemView.findViewById(R.id.price_tv);
+             order_id_tv = (TextView) itemView.findViewById(R.id.order_id_tv);
+             status_tv = (TextView) itemView.findViewById(R.id.status_tv);
+              image_iv =  itemView.findViewById(R.id.image_iv);
+
+
+        }
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/team5/seeshop/customer/MyOrdersActivity.java
+++ app/src/main/java/com/team5/seeshop/customer/MyOrdersActivity.java
@@ -1,16 +1,102 @@
 package com.team5.seeshop.customer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.team5.seeshop.R;
+import com.team5.seeshop.adapters.OrdersAdapter;
+import com.team5.seeshop.models.PlaceOrderModel;
+import com.team5.seeshop.utils.ConstantStrings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyOrdersActivity extends AppCompatActivity {
 
+    public FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    public DatabaseReference databaseReference ;
+
+    List<PlaceOrderModel> productModelList;
+    ProgressBar progressBar;
+
+    RecyclerView recyclerView;
+    SharedPreferences sharedPref;
+    TextView no_found_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_orders);
+
+        getSupportActionBar().hide();
+        sharedPref = getSharedPreferences(ConstantStrings.SEESHOP_PREFS,0);
+
+        databaseReference = mDatabase.getReference(ConstantStrings.ORDERS).child(sharedPref.getString(ConstantStrings.USER_ID,"0"));
+        no_found_tv = findViewById(R.id.no_found_tv);
+        progressBar = findViewById(R.id.progressBar);
+        recyclerView=findViewById(R.id.recyclerView);
+        productModelList=new ArrayList<>();
+        getOrdersData();
     }
+
+
+
+
+
+    /*------------ get All Orders from Firebase ---------------------*/
+    private void getOrdersData() {
+        progressBar.setVisibility(View.VISIBLE);
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    progressBar.setVisibility(View.GONE);
+                    no_found_tv.setVisibility(View.GONE);
+                    recyclerView.setVisibility(View.VISIBLE);
+                    for (DataSnapshot productDs : dataSnapshot.getChildren())
+                    {
+                        PlaceOrderModel productModel2 = productDs.getValue(PlaceOrderModel.class);
+
+                        productModelList.add(productModel2);
+
+                        OrdersAdapter adapter = new OrdersAdapter(productModelList, MyOrdersActivity.this);
+                        recyclerView.setHasFixedSize(true);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(MyOrdersActivity.this));
+                        recyclerView.setAdapter(adapter);
+
+                    }
+
+
+                } else {
+                    progressBar.setVisibility(View.GONE);
+
+                    no_found_tv.setVisibility(View.VISIBLE);
+                    recyclerView.setVisibility(View.GONE);
+                    // Toast.makeText(MyOrdersActivity.this, "No orders found.", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+
+
+
+    }
+    /*---------------------------------------------------------------------*/
 }
