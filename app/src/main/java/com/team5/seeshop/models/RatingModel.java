6075d0d5e6c8bd61f7bdd35a9855699f5cced890
bharghav17 <balu.bharghav17@gmail.com>
2021-07-08 20:34:01 -0400

    Added Rating

---
+++ app/src/main/java/com/team5/seeshop/models/RatingModel.java
@@ -0,0 +1,32 @@
+package com.team5.seeshop.models;
+
+public class RatingModel {
+
+
+    float rating;
+    String name,user_id;
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+}
